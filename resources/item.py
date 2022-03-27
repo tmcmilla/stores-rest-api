f3b79915a4233c4092683d6d1ac527e9e71fdd60
Thomas McMillan <thomasmcmillan@Thomass-MacBook-Pro.local>
2022-03-26 17:32:43 -0700

    First commit, added REST API to access ites, stores, and user authentication.

---
+++ resources/item.py
@@ -0,0 +1,64 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    parser.add_argument('store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store id."
+    )
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'message': 'Item not found'}, 404
+            
+    def post(self, name):
+        if ItemModel.find_by_name(name):        
+            return {'message': f"an item with name '{name}' already exists."}, 400
+
+        data = Item.parser.parse_args()
+                
+        item = ItemModel(name, **data)
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "An error occurred insert the item"}, 500  # internal server error
+        
+        return item.json(), 201
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)    
+        if item is None:
+            item = ItemModel(name, **data)            
+        else:
+            item.price = data['price']
+        item.save_to_db()
+            
+        return item.json(), 201
+
+
+class ItemList(Resource):
+    def get(self):
+        #  Use my javascript or other languages or map reduce
+        # return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
+        # For only python 
+        return {'items': [x.json() for x in ItemModel.query.all()]}
